1e42deffa52c7ecd467fe7f0bd1caddb7043ac07
Brittany Jones <britzay1009@gmail.com>
2023-06-23 01:29:12 +0000

    create_security_group2

---
+++ create_security_group.py
@@ -0,0 +1,11 @@
+import boto3
+
+ec2 = boto3.client('ec2')
+
+response = ec2.create_security_group(
+    Description='SG from boto3',
+    GroupName='boto3-security-group',
+    VpcId='vpc-0b0ab2260d331f8e7',
+)
+
+print(response["GroupId"])
